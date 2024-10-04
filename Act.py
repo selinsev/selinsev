43daa4601cee12ca70f9c8f2415ebefbde226f48
Gouri <92294740+GouriSVipin@users.noreply.github.com>
2024-10-04 22:41:00 +0200

    Act.py

--- Act.py
+++ Act.py
@@ -3,12 +3,14 @@ class Act:
     def __init__(self):
         # List of possible instructions
         self.instructions = [
+            "Touch Thumb to the base of pinky",
             "Touch Thumb with Index",
             "Touch Thumb with Middle",
             "Touch Thumb with Ring",
             "Touch Thumb with Pinky",
             "Open All Fingers",
-            "Close All Fingers"
+            "Close All Fingers",
+            "Move Wrist (Fingers to the wrist)"
         ]
 
     def get_instruction(self, index):
